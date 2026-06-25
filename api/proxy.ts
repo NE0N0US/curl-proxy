cb6fddb3493cd5358aac364c1f7d27bf5352b77c
Neonous <110604908+NE0N0US@users.noreply.github.com>
2026-06-25 17:20:29 +0300

    Fix bugs and code style

--- api/proxy.ts
+++ api/proxy.ts
@@ -1,4 +1,11 @@
-//#region - data
+// #region - imports
+
+import fs from 'fs'
+import path from 'path'
+
+// #endregion
+
+// #region - data
 
 enum HttpStatus {
 	OK = 200,
@@ -20,16 +27,16 @@ enum SearchParam {
 
 const SearchDefaults = Object.freeze({
 	DEL_HEADERS: Object.freeze([
-		//https://developer.mozilla.org/docs/Web/HTTP/Reference/Status/304
+		// https://developer.mozilla.org/docs/Web/HTTP/Reference/Status/304
 		'Cache-Control', 'Pragma', 'If-Modified-Since', 'If-None-Match',
-		//proxy address
+		// proxy address
 		'Origin', 'Referer', 'Forwarded', 'X-Forwarded-For', 'X-Forwarded-Host',
 	]),
 	HEADERS: Object.freeze({
 		'Sec-Fetch-Site': 'same-site',
 	}),
 	DEL_RES_HEADERS: Object.freeze([
-		//proxy sends raw decoded body, except Deno: https://docs.deno.com/runtime/fundamentals/http_server/#automatic-body-compression
+		// proxy sends raw decoded body, except Deno: https://docs.deno.com/runtime/fundamentals/http_server/#automatic-body-compression
 		'Content-Encoding',
 	]),
 	RES_HEADERS: Object.freeze({
@@ -37,32 +44,13 @@ const SearchDefaults = Object.freeze({
 	}),
 })
 
-//#endregion
+const COOKIE_HEADER = 'set-cookie'
 
-//#region - help
+// #endregion
 
-const helpHtml =
-`<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<meta charset="utf-8">
-		<title>URL Proxy</title>
-		<link rel="icon" href="data:image/vnd.microsoft.icon;base64,">
-		<meta name="viewport" content="width=device-width,initial-scale=1">
-		<style>
-			html, body{
-				color-scheme: light dark;
-				overscroll-behavior: none;
-			}
-			pre{
-				white-space: pre-wrap;
-			}
-		</style>
-	</head>
-	<body>
-		<pre><code>HELP_TEXT</code></pre>
-	</body>
-</html>`
+// #region - help
+
+let helpHtml
 
 function formatStringArray(array: string[]) {
 	return `["${array.join('", "')}"]`
@@ -72,7 +60,7 @@ function formatStringRecord(record: Record<string, unknown>) {
 	return JSON.stringify(record, undefined, ' ').replace(/(?:(?<={)\n )|\n/g, '')
 }
 
-/**https://lodash.com/docs/#escape*/
+/** https://lodash.com/docs/#escape */
 function escapeHtml(text: string) {
 	return text.replace(/[&<>"']/g, char => `&#${char.charCodeAt(0)};`)
 }
@@ -82,7 +70,7 @@ function formatHelp(message?: string, serviceUrl = 'http://localhost:3000/', htm
 		url = new URL(serviceUrl),
 		width = Math.max(...Object.values(SearchParam).map(({length}) => length)),
 		text = `${message ? message + '\n\n' : ''}` +
-			//usage
+			// usage
 			`Usage:\n${url.origin}${url.pathname}?` +
 			`${SearchParam.URL}=<url>` +
 			`[&${SearchParam.HEADERS}=<json_object>]` +
@@ -93,44 +81,49 @@ function formatHelp(message?: string, serviceUrl = 'http://localhost:3000/', htm
 			`[&${SearchParam.STATUS}=<status_code>]` +
 			`[&${SearchParam.STATUS_TEXT}=<status_message>]` +
 			`[&${SearchParam.TIMEOUT}=<milliseconds>]` +
-			//url params
+			// url params
 			`\n\nURL Parameters:\n` +
 			`* ${SearchParam.URL.padEnd(width)} - original resource URL, default protocol is https\n` +
-			//headers
+			// headers
 			`* ${SearchParam.HEADERS.padEnd(width)} - request headers to overwrite` +
 			(isRecord(SearchDefaults.HEADERS, String, 'string') ? ', in addition to:\n' : '\n') +
 			(isRecord(SearchDefaults.HEADERS, String, 'string') ? `  ${formatStringRecord(SearchDefaults.HEADERS)}\n` : '') +
-			//delheaders
+			// delheaders
 			`* ${SearchParam.DEL_HEADERS.padEnd(width)} - names of request headers to delete` +
 			(isArray(SearchDefaults.DEL_HEADERS, String) ? ', in addition to:\n' : '\n') +
 			(isArray(SearchDefaults.DEL_HEADERS, String) ? `  ${formatStringArray(SearchDefaults.DEL_HEADERS)}\n` : '') +
-			//resheaders
+			// resheaders
 			`* ${SearchParam.RES_HEADERS.padEnd(width)} - response headers to overwrite` +
 			(isRecord(SearchDefaults.RES_HEADERS, String, 'string') ? ', in addition to:\n' : '\n') +
 			(isRecord(SearchDefaults.RES_HEADERS, String, 'string') ? `  ${formatStringRecord(SearchDefaults.RES_HEADERS)}\n` : '') +
-			//delresheaders
+			// delresheaders
 			`* ${SearchParam.DEL_RES_HEADERS.padEnd(width)} - names of response headers to delete` +
 			(isArray(SearchDefaults.DEL_RES_HEADERS, String) ? ', in addition to:\n' : '\n') +
 			(isArray(SearchDefaults.DEL_RES_HEADERS, String) ? `  ${formatStringArray(SearchDefaults.DEL_RES_HEADERS)}\n` : '') +
-			//other params
+			// other params
 			(`* ${SearchParam.SKIP_DEFAULTS.padEnd(width)} - do not apply default header changes\n`) +
 			`* ${SearchParam.STATUS.padEnd(width)} - response status code to overwrite\n` +
 			`* ${SearchParam.STATUS_TEXT.padEnd(width)} - response status message to overwrite\n` +
 			`* ${SearchParam.TIMEOUT.padEnd(width)} - milliseconds to abort request after`
-	return html ? helpHtml.replace('HELP_TEXT', escapeHtml(text)) : text
+	return html ? (helpHtml ??= fileText('templates/help.html'))
+		.replace('HELP_TEXT', escapeHtml(text)) : text
 }
 
 function helpResponse(req: Request, status = HttpStatus.OK, message?: string) {
 	const html = req.headers.get('accept')?.includes('text/html')
 	return new Response(formatHelp(message, req.url, html), {
 		status,
-		headers: {'Content-Type': 'text/html'},
+		headers: html ? {'Content-Type': 'text/html'} : undefined,
 	})
 }
 
-//#endregion
+// #endregion
 
-//#region - utils
+// #region - utils
+
+function fileText(filename: string) {
+	return fs.readFileSync(path.join(process.cwd(), filename)).toString()
+}
 
 function isArray<T = any>(value: any, ofClass?: Function): value is T[] {
 	return Array.isArray(value) && (!ofClass || !!value.length &&
@@ -171,7 +164,7 @@ function escapeRegex(text: string) {
 	return text.replace(/[\^\$\.\*\+\?\(\)\[\]\{\}\|]/g, char => '\\' + char)
 }
 
-function deleteWildcard(record: Record<string, string>, key: string) {
+function deleteWildcard<T>(record: Record<string, T>, key: string) {
 	const regex = new RegExp('^' + key.replace(/./g, char =>
 		char === '*' ? '.*' : escapeRegex(char)
 	) + '$', 'i')
@@ -182,17 +175,17 @@ function deleteWildcard(record: Record<string, string>, key: string) {
 	return record
 }
 
-function lowerKeys(record: Record<string, string>): typeof record {
+function lowerKeys<T>(record: Record<string, T>): typeof record {
 	return Object.fromEntries(
 		Object.entries(record).map(([k, v]) => [k.toLowerCase(), v])
 	)
 }
 
-//#endregion
+// #endregion
 
-//#region - proxy
+// #region - proxy
 
-async function proxy(req: Request) {
+async function proxy(req: Request, timerSuffix: number) {
 	const {
 		[SearchParam.URL]: url,
 		[SearchParam.SKIP_DEFAULTS]: clean,
@@ -200,15 +193,15 @@ async function proxy(req: Request) {
 	} = Object.fromEntries(new URL(req.url).searchParams)
 	if (!url)
 		return helpResponse(req, HttpStatus.BAD_REQUEST, `Missing ${SearchParam.URL} parameter!`)
-	//get request headers
+	// get request headers
 	const reqHeaders = Object.fromEntries(req.headers)
-	//delete request headers
+	// delete request headers
 	;[
 		...clean !== undefined ? [] : SearchDefaults.DEL_HEADERS,
 		...tryParse<string[]>(params[SearchParam.DEL_HEADERS], isArray, String) ?? [],
 	]
 		?.forEach(name => deleteWildcard(reqHeaders, name))
-	//overwrite request headers
+	// overwrite request headers
 	Object.assign(reqHeaders,
 		clean !== undefined ? [] : lowerKeys(SearchDefaults.HEADERS ?? {}),
 		lowerKeys(Object.fromEntries(new Headers(
@@ -216,34 +209,43 @@ async function proxy(req: Request) {
 		)))
 	)
 	try {
-		console.time('fetch')
+		console.time('fetch-' + timerSuffix)
 		const
 			timeoutParam = params[SearchParam.TIMEOUT],
-			timeout = Math.min(0, Number.isSafeInteger(+timeoutParam) ? +timeoutParam : 0),
-		//make response
+			timeout = Math.max(0, Number.isSafeInteger(+timeoutParam) ? +timeoutParam : 0),
+		// get response
 			res = await fetch(url.match(/^\w+:\/\//) ? url : ('https://' + url), new Request(req, {
 				headers: new Headers(reqHeaders),
 				signal: timeout ? AbortSignal.timeout(timeout) : undefined,
 			})),
-		//get response headers
-			headers = Object.fromEntries(res.headers)
-		console.timeEnd('fetch')
-		//delete response headers
+		// get response headers
+			resHeaders: Record<string, string | string[]> = Object.fromEntries(res.headers)
+		console.timeEnd('fetch-' + timerSuffix)
+		// get cookies
+		if (res.headers.getSetCookie().length)
+			resHeaders[COOKIE_HEADER] = res.headers.getSetCookie()
+		// delete response headers
 		;[
 			...clean !== undefined ? [] : SearchDefaults.DEL_RES_HEADERS,
 			...tryParse<string[]>(params[SearchParam.DEL_RES_HEADERS], isArray, String) ?? [],
 		]
-			?.forEach(name => deleteWildcard(headers, name))
-		//overwrite response headers
-		Object.assign(headers,
+			?.forEach(name => deleteWildcard(resHeaders, name))
+		// overwrite response headers
+		Object.assign(resHeaders,
 			clean !== undefined ? [] : lowerKeys(SearchDefaults.RES_HEADERS ?? {}),
 			lowerKeys(Object.fromEntries(new Headers(
 				tryParse<Record<string, string>>(params[SearchParam.RES_HEADERS], isRecord, String, 'string')
 			)))
 		)
-		//clone response
+		// set cookies
+		const headers = new Headers(resHeaders as Record<string, string>)
+		if (isArray(resHeaders[COOKIE_HEADER], String)) {
+			headers.delete(COOKIE_HEADER)
+			resHeaders[COOKIE_HEADER].forEach(value => headers.append(COOKIE_HEADER, value))
+		}
+		// clone response
 		return new Response(res.body, {
-			headers: new Headers(headers),
+			headers,
 			status: +(params[SearchParam.STATUS] || res.status),
 			statusText: params[SearchParam.STATUS_TEXT] ?? res.statusText,
 		})
@@ -256,14 +258,16 @@ async function proxy(req: Request) {
 	}
 }
 
+let c = 0
+
 export default {
 	fetch: async (req: Request) => {
-		console.time('proxy')
-		const res = await proxy(req)
-		console.timeEnd('proxy')
-		console.groupEnd()
+		const n = c++
+		console.time('proxy-' + n)
+		const res = await proxy(req, n)
+		console.timeEnd('proxy-' + n)
 		return res
 	}
 }
 
-//#endregion
+// #endregion
